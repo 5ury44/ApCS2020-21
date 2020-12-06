c7f87b23690f9876138fba5e18f05ab445052cf8
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2020-12-06 12:12:26 -0500

    Update Parse.java

--- src/Parse.java
+++ src/Parse.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
 public class Parse {
-    static class equalException extends Exception{public equalException(String x){super(x);}}
+    static class equalException extends Exception{public equalException(String x){super(x);}} //create a new custom exception
     static class noExpressionException extends Exception{public noExpressionException(String x){super(x);}}
+    static ArrayList<String> split(String str, ArrayList<String> del){
+        ArrayList<String> result = new ArrayList<>(Arrays.asList(""));
+        for (int i = 0; i < str.length(); i++){if(!del.contains(Character.toString(str.charAt(i))))result.add(Character.toString(str.charAt(i)));} //alternative for split
+        return result;
+    }
     public static void main(String[] args) {
         Set<String> signs = new HashSet<>(Arrays.asList("+","-","*","/","="));
         try {
@@ -11,19 +16,20 @@ public class Parse {
             Scanner scanner = new Scanner(System.in);
             System.out.println("please enter the expression");
             String input = scanner.nextLine();
-            List<String> elementsExp = new ArrayList<>(Arrays.asList(input.split("=-|=\\+|=|\\+|-|\\*|/")));
-            List<String> Elements = new ArrayList<>(Arrays.asList(input.split("")));
+            List<String> elementsExp = Parse.split(input, new ArrayList<>(Arrays.asList("-","+","=","*","/"))); //input splitting w/out signs
+            List<String> Elements = Parse.split(input, new ArrayList<>(Arrays.asList(""))); //input split w/ all signs
+            Elements.remove(0);
             if(!Elements.get(0).equals("=")){throw new equalException(input);}
-            if(elementsExp.size()<=2){throw new noExpressionException(input);}
+            if(elementsExp.size()<=2){throw new noExpressionException(input);} //check for errors of no expression or no equal sign
             for(int i=1; i<Elements.size();i++){
                 if((Elements.get(i-1).equals("*")&&Elements.get(i).equals("-"))){Elements.set(i,"Neg"); Elements.remove(i-1);}
                 else if((Elements.get(i-1).equals("/")&&Elements.get(i).equals("-"))){Elements.set(i,"Ng"); Elements.remove(i-1);}
-                else if((Elements.get(i-1).equals("-")&&Elements.get(i).equals("-"))){Elements.set(i,"+"); Elements.remove(i-1);}
+                else if((Elements.get(i-1).equals("-")&&Elements.get(i).equals("-"))){Elements.set(i,"+"); Elements.remove(i-1);} //replace +-, --, *-, or /- with a single sign
                 else if((Elements.get(i-1).equals("+")&&Elements.get(i).equals("-"))){Elements.set(i,"-"); Elements.remove(i-1);}
             }
             for(int i=1; i<Elements.size();i++){
                 if(signs.contains(Elements.get(i))&&(signs.contains(Elements.get(i-1))||i==Elements.size()-1)){
-                    if(((Elements.get(i).equals("-"))&&Elements.get(i-1).equals("="))){continue;}
+                    if(((Elements.get(i).equals("-"))&&Elements.get(i-1).equals("="))){continue;} //check if not expression
                     throw new noExpressionException(input);
                 }
             }
@@ -31,16 +37,16 @@ public class Parse {
             for(String s:Elements){
                 try{Double.parseDouble(s);}
                 catch(Exception e){
-                    if(signs.contains(s)||s.equals("Neg")||s.equals("Ng")){oppList.add(s);}
+                    if(signs.contains(s)||s.equals("Neg")||s.equals("Ng")){oppList.add(s);}  //add to operation list
                     else if(!s.equals(".")){throw new noExpressionException(input);}
             }
             }
             oppList.remove("=");
-            parse.calculate(elementsExp,oppList);
+            parse.calculate(elementsExp,oppList); //calculate with numbers and signs lists
             scanner.close();
         }
         catch(equalException e){System.out.print(e.getMessage()+": no equal sign.");}
-        catch(noExpressionException e){System.out.print(e.getMessage()+" is not a valid expression.");}
+        catch(noExpressionException e){System.out.print(e.getMessage()+" is not a valid expression.");} //print statements when exceptions caught
         catch(Exception e){System.out.println("Encountered an error.");}
     }
 }
