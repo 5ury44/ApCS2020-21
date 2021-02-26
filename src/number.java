21c8d43bd8b1c907f67e08ff0f7e2495ef069cc0
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2021-02-26 00:26:13 -0500

    Create number.java

---
+++ src/number.java
@@ -0,0 +1,6 @@
+public interface number{
+    public number add(number num);
+    public number subtract(number num);
+    public number multiply(number num);
+    public number divide(number num);
+}
