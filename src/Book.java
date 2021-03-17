10ee3e3b4276530ce0f8b02e1072aa6ae6b069f7
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2021-03-17 08:04:01 -0400

    Delete Book.java

--- src/Book.java
+++
@@ -1,9 +0,0 @@
-public class Book {
-    String title, author, name, date;
-    public Book(String title, String author, String date, String name){
-        this.title=title;
-        this.author=author;
-        this. name=name;
-        this.date=date;
-    }
-}
