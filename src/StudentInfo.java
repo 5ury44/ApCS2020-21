2b6ea8dadf52082ec5493901e11b5d6db673a871
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2021-03-17 08:42:27 -0400

    Create StudentInfo.java

---
+++ src/StudentInfo.java
@@ -0,0 +1,9 @@
+public class StudentInfo {
+    private String schoolClass, name;
+    private int avg;
+    public StudentInfo(String name, int avg, String schoolClass){
+        this.avg=avg;
+        this.schoolClass=schoolClass;
+        this.name=name;
+    }
+}
