b4722dadc7f093e8256551bbfabc255862a3fae0
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2021-03-17 08:39:16 -0400

    Delete StudentInfo.java

--- src/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-public class StudentInfo {
-    private String schoolClass, name;
-    private int avg;
-    public StudentInfo(String name, int avg, String schoolClass){
-        this.avg=avg;
-        this.schoolClass=schoolClass;
-        this.name=name;
-    }
-}
